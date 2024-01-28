924c1ce3b648c552540417abfb2b90b82b3f9e2a
Andrej Marčan <yami.yatem@gmail.com>
2024-01-28 20:33:56 +0100

    feat: YGO command for latest banlist

--- src/main/java/com/yatensoft/dcbot/constant/WebsiteParserConstant.java
+++ src/main/java/com/yatensoft/dcbot/constant/WebsiteParserConstant.java
@@ -8,8 +8,7 @@ public class WebsiteParserConstant {
     public static final String FABTCG_ITEM_LINK_CLASS = "item-link";
 
     /** yugioh-card.com Constants */
-    public static final String YGOCARD_BLOCK_BUTTON_CLASS =
-            "wp-block-button__link"; // "wp-block-button__link has-text-color has-background";
+    public static final String YGOCARD_BLOCK_BUTTON_CLASS = "wp-block-button__link";
 
     /** Common Constants */
     public static final String HREF = "href";
